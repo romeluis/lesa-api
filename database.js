4cbfa218e7fe3f58be0f493c5cb9f2cc6ed2760e
romeluis <113552563+romeluis@users.noreply.github.com>
2024-08-19 17:21:01 -0400

    hid passwords

--- database.js
+++ database.js
@@ -1,9 +1,12 @@
 import mysql from "mysql";
 
+const SQL_USER = process.env.SQL_USER;
+const SQL_PASS = process.env.SQL_PASS;
+
 export const database = mysql.createConnection({
     host: "server329.web-hosting.com",
     port: "3306",
     database: "lesaueqw_lesadb",
-    user: "lesaueqw_admin",
-    password: "Lu!sB3atriz"
+    user: SQL_USER,
+    password: SQL_PASS
 });
