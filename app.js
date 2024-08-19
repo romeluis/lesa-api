318d6684413e17c74f4b7ef98c29267cfb8a07e8
romeluis <113552563+romeluis@users.noreply.github.com>
2024-08-19 17:07:33 -0400

    added listening to port

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import express from "express";
 import eventsRoutes from "./routes/events.js";
 
+const PORT = process.env.PORT || 8800;
+
+//Set up API and routes
 const app = express();
 app.use(express.json());
 app.use("/events", eventsRoutes)
+
+//Message for unspecified access
+app.get("/", (req, res) => {
+    return res.json("Welcome to the LESA API");
+});
+
+//Listen for requests
+app.listen(PORT, () => {
+    console.log("Connected...");
+});
