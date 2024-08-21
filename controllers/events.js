701d408e3e201a12ff17d0ba86c2f2bcc71a785a
romeluis <113552563+romeluis@users.noreply.github.com>
2024-08-21 17:22:31 -0400

    added month query

--- controllers/events.js
+++ controllers/events.js
@@ -3,12 +3,23 @@ import {database} from "../database.js";
 export const getEvents = (request, response) => {
     response.set('Access-Control-Allow-Origin', '*');
 
-    const query = request.query.type ? "SELECT * FROM events WHERE type=?" : "SELECT * FROM events";
-
-    database.query(query, [request.query.type], (error, data) => {
-        if (error) return response.send(error);
-        return response.status(200).json(data);
-    });
+    const query = request.query.type ? "SELECT * FROM events WHERE type=?" : (request.query.month ? "SELECT * FROM events WHERE month=?" : "SELECT * FROM events");
+    if (request.query.type) {
+        database.query(query, [request.query.type], (error, data) => {
+            if (error) return response.send(error);
+            return response.status(200).json(data);
+        });
+    } else if (request.query.month) {
+        database.query(query, [request.query.month], (error, data) => {
+            if (error) return response.send(error);
+            return response.status(200).json(data);
+        });
+    } else {
+        database.query(query, [], (error, data) => {
+            if (error) return response.send(error);
+            return response.status(200).json(data);
+        });
+    }
 }
 
 export const getEvent = (request, response) => {
