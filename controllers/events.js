a84669b736aabbaf090714bfa9b4bc2e93cfefcc
romeluis <113552563+romeluis@users.noreply.github.com>
2024-08-21 15:34:15 -0400

    added code for getting event info

--- controllers/events.js
+++ controllers/events.js
@@ -1,6 +1,8 @@
 import {database} from "../database.js";
 
 export const getEvents = (request, response) => {
+    response.set('Access-Control-Allow-Origin', '*');
+
     const query = request.query.type ? "SELECT * FROM events WHERE type=?" : "SELECT * FROM events";
 
     database.query(query, [request.query.type], (error, data) => {
@@ -10,5 +12,12 @@ export const getEvents = (request, response) => {
 }
 
 export const getEvent = (request, response) => {
-    
+    response.set('Access-Control-Allow-Origin', '*');
+
+    const query = "SELECT * FROM events WHERE id=?";
+
+    database.query(query, [request.params.id], (error, data) => {
+        if (error) return response.send(error);
+        return response.status(200).json(data);
+    });
 }
