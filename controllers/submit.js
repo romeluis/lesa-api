97181f5ba97df1efcada893939ad8ca4d3bcafce
Romel Luis Faife Cruz <113552563+romeluis@users.noreply.github.com>
2025-07-29 20:26:02 +0000

    Ensured one student id entry

--- controllers/submit.js
+++ controllers/submit.js
@@ -18,27 +18,55 @@ export const submitJoin = (req, res) => {
         country,
     } = req.body;
 
-    const q =
-        "INSERT INTO members (`given_name`, `surname_name`, `preferred_name`, `uoft_email`, `student_number`, `student_status`, `faculty`, `college`, `program`, `year_of_study`, `country`, `registration_date`) VALUES (?)";
+    const q_select = "SELECT * FROM members WHERE student_number = ?";
 
-    const values = [
-        given_name,
-        surname_name,
-        preferred_name,
-        uoft_email,
-        student_number,
-        student_status,
-        faculty,
-        college,
-        program,
-        year_of_study,
-        country,
-        new Date(),
-    ];
-
-    database.query(q, [values], (err, data) => {
+    database.query(q_select, [student_number], (err, data) => {
         if (err) return res.status(500).json(err);
-        return res.json({ message: "User has been created." });
+
+        if (data.length) {
+            // User exists, update
+            const q_update =
+                "UPDATE members SET `given_name` = ?, `surname_name` = ?, `preferred_name` = ?, `uoft_email` = ?, `student_status` = ?, `faculty` = ?, `college` = ?, `program` = ?, `year_of_study` = ?, `country` = ? WHERE student_number = ?";
+            const values_update = [
+                given_name,
+                surname_name,
+                preferred_name,
+                uoft_email,
+                student_status,
+                faculty,
+                college,
+                program,
+                year_of_study,
+                country,
+                student_number,
+            ];
+            database.query(q_update, values_update, (err, data) => {
+                if (err) return res.status(500).json(err);
+                return res.json({ message: "User has been updated." });
+            });
+        } else {
+            // User does not exist, insert
+            const q_insert =
+                "INSERT INTO members (`given_name`, `surname_name`, `preferred_name`, `uoft_email`, `student_number`, `student_status`, `faculty`, `college`, `program`, `year_of_study`, `country`, `registration_date`) VALUES (?)";
+            const values_insert = [
+                given_name,
+                surname_name,
+                preferred_name,
+                uoft_email,
+                student_number,
+                student_status,
+                faculty,
+                college,
+                program,
+                year_of_study,
+                country,
+                new Date(),
+            ];
+            database.query(q_insert, [values_insert], (err, data) => {
+                if (err) return res.status(500).json(err);
+                return res.json({ message: "User has been created." });
+            });
+        }
     });
 };
 
