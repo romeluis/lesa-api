1cd26696bc19b609fc32d22c28da6414d478209c
Romel Luis Faife Cruz <113552563+romeluis@users.noreply.github.com>
2025-07-30 00:36:52 +0000

    Added submit event

--- controllers/submit.js
+++ controllers/submit.js
@@ -1,6 +1,25 @@
 import { database } from "../database.js";
 
 export const submitEvent = (req, res) => {
+    const event_id = req.params.id;
+    const { student_id } = req.body;
+
+    // Check if student exists
+    const q_select = "SELECT * FROM members WHERE student_number = ?";
+    database.query(q_select, [student_id], (err, data) => {
+        if (err) return res.status(500).json(err);
+        if (data.length === 0) {
+            return res.status(404).json({ message: "Student not found." });
+        }
+
+        // Student exists, register for event
+        const student_member_id = data[0].id;
+        const q_insert = "INSERT INTO event_registration (`event_id`, `student_id`) VALUES (?, ?)";
+        database.query(q_insert, [event_id, student_member_id], (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.json({ message: "Student registered for event." });
+        });
+    });
 }
 
 export const submitJoin = (req, res) => {
